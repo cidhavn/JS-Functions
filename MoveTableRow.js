d7e0fdb40382c1169a3e1d58d5729b87e006aa16
cidhavn <cidhavn@hotmail.com>
2014-12-22 14:55:53 +0800

    Update MoveTableRow.js

--- MoveTableRow.js
+++ MoveTableRow.js
@@ -1,11 +1,13 @@
-//sample:
-//function MoveUp() { MoveTableRow("chkSelect", 2, true); }
-//function MoveDown() { MoveTableRow("chkSelect", 2, false); }
-//<table><tr><td><input type="checkbox" name="chkSelect" /></td></tr></table>
+// Move Table Row with JQuery
+// ----------------------------
+// sample:
+// function MoveUp() { MoveTableRow("chkSelect", 2, true); }
+// function MoveDown() { MoveTableRow("chkSelect", 2, false); }
+// <table><tr><td><input type="checkbox" name="chkSelect" /></td></tr></table>
 
-//checkboxsName: 選取checkbox name
-//parentCount: Row最上層<tr>階層數
-//isUp: [true = up][false = down]
+// checkboxsName: 選取checkbox name
+// parentCount: Row最上層<tr>階層數
+// isUp: [true = up][false = down]
 function MoveTableRow(checkboxsName, parentCount, isUp) {
     var checkboxs = document.getElementsByName(checkboxsName);
     var checkeds = [];
